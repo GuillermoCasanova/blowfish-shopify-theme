0dc487e7a0b2015e430182dc3765a4644c50b9e9
Guillermo Casanova <Casanova@Guillermos-MBP.local>
2019-08-21 09:24:37 -0400

    + The first commit

---
+++ src/scripts/sections/product.js
@@ -0,0 +1,193 @@
+/**
+ * Product Template Script
+ * ------------------------------------------------------------------------------
+ * A file that contains scripts highly couple code to the Product template.
+ *
+   * @namespace product
+ */
+
+theme.Product = (function() {
+
+  var selectors = {
+    addToCart: '[data-add-to-cart]',
+    addToCartText: '[data-add-to-cart-text]',
+    comparePrice: '[data-compare-price]',
+    comparePriceText: '[data-compare-text]',
+    originalSelectorId: '[data-product-select]',
+    optionSelector: '[data-option-selector]',
+    priceWrapper: '[data-price-wrapper]',
+    productFeaturedImage: '[data-product-featured-image]',
+    productJson: '[data-product-json]',
+    productPrice: '[data-product-price]',
+    productThumbs: '[data-product-single-thumbnail]',
+    qtyButtons: '[data-qty-counter-button]',
+    singleOptionSelector: '[data-single-option-selector]'
+  };
+
+  /**
+   * Product section constructor. Runs on page load as well as Theme Editor
+   * `section:load` events.
+   * @param {string} container - selector for the section container DOM element
+   */
+  function Product(container) {
+    this.$container = $(container);
+
+    // Stop parsing if we don't have the product json script tag when loading
+    // section in the Theme Editor
+    if (!$(selectors.productJson, this.$container).html()) {
+      return;
+    }
+
+    var sectionId = this.$container.attr('data-section-id');
+    this.productSingleObject = JSON.parse($(selectors.productJson, this.$container).html());
+
+    var options = {
+      $container: this.$container,
+      enableHistoryState: this.$container.data('enable-history-state') || false,
+      singleOptionSelector: selectors.singleOptionSelector,
+      originalSelectorId: selectors.originalSelectorId,
+      product: this.productSingleObject
+    };
+
+    this.settings = {};
+    this.namespace = '.product';
+    this.variants = new slate.Variants(options);
+    this.$featuredImage = $(selectors.productFeaturedImage, this.$container);
+
+    this.$container.on('variantChange' + this.namespace, this.updateAddToCartState.bind(this));
+    this.$container.on('variantPriceChange' + this.namespace, this.updateProductPrices.bind(this));
+
+    if (this.$featuredImage.length > 0) {
+      this.settings.imageSize = slate.Image.imageSize(this.$featuredImage.attr('src'));
+      slate.Image.preload(this.productSingleObject.images, this.settings.imageSize);
+
+      this.$container.on('variantImageChange' + this.namespace, this.updateProductImage.bind(this));
+    }
+
+
+    this.$optionSelector = $(selectors.optionSelector);
+
+
+    this.$optionSelector.change(function() {
+      var optionValue = $(this).val();
+      $(this)
+        .closest('form')
+        .find('[data-single-option-selector]')
+        .val(optionValue)
+        .trigger('change');
+    });
+
+    this.initSecondaryInfo(); 
+
+    this.initQtyBtns(); 
+
+  }
+
+  Product.prototype = $.extend({}, Product.prototype, {
+
+    /**
+     * Updates the DOM state of the add to cart button
+     *
+     * @param {boolean} enabled - Decides whether cart is enabled or disabled
+     * @param {string} text - Updates the text notification content of the cart
+     */
+    updateAddToCartState: function(evt) {
+      var variant = evt.variant;
+
+      if (variant) {
+        $(selectors.priceWrapper, this.$container).removeClass('hide');
+      } else {
+        $(selectors.addToCart, this.$container).prop('disabled', true);
+        $(selectors.addToCartText, this.$container).html(theme.strings.unavailable);
+        $(selectors.priceWrapper, this.$container).addClass('hide');
+        return;
+      }
+
+      if (variant.available) {
+        $(selectors.addToCart, this.$container).prop('disabled', false);
+        $(selectors.addToCartText, this.$container).html(theme.strings.addToCart);
+      } else {
+        $(selectors.addToCart, this.$container).prop('disabled', true);
+        $(selectors.addToCartText, this.$container).html(theme.strings.soldOut);
+      }
+    },
+
+    /**
+     * Updates the DOM with specified prices
+     *
+     * @param {string} productPrice - The current price of the product
+     * @param {string} comparePrice - The original price of the product
+     */
+    updateProductPrices: function(evt) {
+      var variant = evt.variant;
+      var $comparePrice = $(selectors.comparePrice, this.$container);
+      var $compareEls = $comparePrice.add(selectors.comparePriceText, this.$container);
+
+      $(selectors.productPrice, this.$container)
+        .html(slate.Currency.formatMoney(variant.price, theme.moneyFormat));
+
+      if (variant.compare_at_price > variant.price) {
+        $comparePrice.html(slate.Currency.formatMoney(variant.compare_at_price, theme.moneyFormat));
+        $compareEls.removeClass('hide');
+      } else {
+        $comparePrice.html('');
+        $compareEls.addClass('hide');
+      }
+    },
+
+    /**
+     * Updates the DOM with the specified image URL
+     *
+     * @param {string} src - Image src URL
+     */
+    updateProductImage: function(evt) {
+      var variant = evt.variant;
+      var sizedImgUrl = slate.Image.getSizedImageUrl(variant.featured_image.src, this.settings.imageSize);
+
+      this.$featuredImage.attr('src', sizedImgUrl);
+    },
+
+    /**
+     * Event callback for Theme Editor `section:unload` event
+     */
+    onUnload: function() {
+      this.$container.off(this.namespace);
+    },
+
+    initSecondaryInfo: function() {
+      if(this.$container.find('.product__information__description__secondary-dscrp')) {
+
+        var $secondaryInfo = $('.product__information__description__secondary-dscrp');
+
+        $secondaryInfo.before('<button class="product__description__toggle" data-secondary-info-toggle>+ More Information</button>'); 
+
+        var $infoToggle = $('[data-secondary-info-toggle]'); 
+
+        $infoToggle.on('click', function() {
+          $secondaryInfo.find('span').toggle();
+          $secondaryInfo.toggle(); 
+        });
+      }
+    },
+
+    initQtyBtns: function() {
+
+      var $qtyButtons = $(selectors.qtyButtons); 
+      var $qty = $('#Quantity'); 
+
+      $qtyButtons.on('click', function(event) {
+        event.preventDefault(); 
+        var currentQty = parseInt($qty.val());
+
+        if(currentQty === 0 && $(this).attr('value') =='-1') {
+          return 
+        }
+        $qty.val(currentQty + parseInt($(this).attr('value')));
+
+      }); 
+    }
+
+  });
+
+  return Product;
+})();
